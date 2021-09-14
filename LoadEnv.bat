65beb093056fb958918adb58e03dfe62f53b671d
git <jack@sabine.ws>
2021-09-14 14:35:35 -0400

    Rename ProgEnv to LoadEnv; can be set up with variables at top of batch file.

--- LoadEnv.bat
+++ LoadEnv.bat
@@ -1,9 +1,10 @@
 set anaconda_dir=C:\Users\jack\anaconda3
 set vscode_dir=C:\Users\jack\AppData\Local\Programs\Microsoft VS Code
 set project_dir=C:\Users\jack\OneDrive\Documents\GitHub\Breadboard_CPU\Programming
+set envname=budgetlake
 
-cd Programming/
+cd %project_dir%
 call "%anaconda_dir%\Scripts\activate.bat" "%anaconda_dir%"
-call conda activate budgetlake
+call conda activate %envname%
 start "" "%vscode_dir%\Code.exe" -r "%project_dir%"
 exit
