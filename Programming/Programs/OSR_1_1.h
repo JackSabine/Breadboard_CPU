f6a97b45b42ca2092a4e80440baad7d1a911e7e6
Jack Sabine <jack@sabine.ws>
2021-12-13 15:52:07 -0500

    edit header file comments

--- Programming/Programs/OSR_1_1.h
+++ Programming/Programs/OSR_1_1.h
@@ -1,6 +1,9 @@
+// Trap table
 #define WAIT    0x0100
 #define SPRINT  0x0101
 
+// Port registers
+#define PADIR   0x00    // Not implemented in hardware
 #define PBDIR   0x01
 #define PORTA   0x02
 #define PORTB   0x03
